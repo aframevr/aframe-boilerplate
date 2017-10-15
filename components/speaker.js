25bffc340cf9bb59c17f9d752410c8ea452f08af
Kaitlyn Reese <kaitlynjreese@gmail.com>
2017-10-15 00:01:55 -0400

    Add speaker

--- components/speaker.js
+++ components/speaker.js
@@ -1,9 +1,9 @@
-AFRAME.registerComponent('room', {
+AFRAME.registerComponent('speaker', {
   init: function () {
     var el = this.el;
     var loader = LOADERS.collada;
     loader.options.convertUpAxis = true;
-    loader.load('assets/nbc_room/model.dae', function (collada) {
+    loader.load('assets/speaker/model.dae', function (collada) {
       el.setObject3D('mesh', collada.scene);
     });
   }
